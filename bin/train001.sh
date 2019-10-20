49d34fa7fd61f676b5f8a60e7987ca9671139f86
htygithub <htygithub@users.noreply.github.com>
2019-10-20 23:24:45 +0800

    Update train001.sh

--- bin/train001.sh
+++ bin/train001.sh
@@ -3,4 +3,4 @@ gpu=0
 fold=0
 conf=./conf/${model}.py
 
-python -m src.cnn.main train ${conf} --fold ${fold} --gpu ${gpu}
+/data/anaconda3/bin/python -m src.cnn.main train ${conf} --fold ${fold} --gpu ${gpu}
